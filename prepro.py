5a327bd39fcccb6feb2f95d6bc0ec4cf555376ec
poojan124 <poojan124@hotmail.com>
2017-10-09 08:09:36 +0530

    Prepro file completed and described each method init

--- prepro.py
+++ prepro.py
@@ -5,6 +5,8 @@ import warnings
 import re,string
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from collections import Counter
+from sklearn.model_selection  import train_test_split
 
 # -*- coding: utf-8 -*-
 def isEnglish(s):
@@ -17,7 +19,27 @@ def isEnglish(s):
 
 #global Data
 words = set(stopwords.words('english'))
-        
+
+'''
+Description of all Methods:
+---------------------------
+-->get_clean : Purpose : Read files and remove uneccesory files. - Parameter : None - Return : pandas dataframe object   
+
+-->process : Purpose : Parse html code into text using beautiful soup- Parameters : pandas dataframe object- Return : pandas dataframe object
+
+-->remove_punc : Purpose : remove punctuation and all numerical numbers from string leaving string with only alphabates
+                 Parameters : pandas dataframe - Returns : pandas dataframe
+
+-->stop_rem : helper method
+                 
+-->remove_stopwords : Purpose : remoce stop words using NLTK library and stop_rem helper method - Parameters : Pandas
+                    dataframe obj - Return : pandas dataframe    
+
+-->feature selection : Purpose : using term frequecy of a word in corpus select some most frequent word as features - 
+                      Parameters : Pandas dataframe obj - Return : pandas dataframe    
+
+'''
+       
 def get_clean():
     files = ['Youtube01-Psy.csv','Youtube02-KatyPerry.csv','Youtube03-LMFAO.csv','Youtube04-Eminem.csv',
             'Youtube05-Shakira.csv']
@@ -28,7 +50,6 @@ def get_clean():
     print(data.shape)
     return data
 
-
 def process(data): 
     warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
     data['CONTENT'] = data['CONTENT'].apply(lambda x: BeautifulSoup(x,'html.parser').get_text())
@@ -37,22 +58,39 @@ def process(data):
 def stop_rem(s):
     new_s = ''
     word_list = word_tokenize(s)
+    word_list = ["_link_feature_" if x.find('http')!=-1 else x for x in word_list]
+    word_list = ["_link_feature_" if x.find('watch')!=-1 and len(x)>9 else x for x in word_list]
     for w in word_list :
         if w.lower() not in words and isEnglish(w):
             new_s = new_s + w.lower() + ' '
     return new_s
     
 def remove_punc(data):
-    data['CONTENT'] = data['CONTENT'].apply(lambda x: re.sub('[%s]' % re.escape(string.punctuation), '', x))
+    data['CONTENT'] = data['CONTENT'].apply(lambda x: re.sub('[^a-zA-Z ]', '', x))
     return data
 
 def remove_stopwords(data):
     data['CONTENT'] = data['CONTENT'].apply(lambda x: stop_rem(x))
-    print(data.head())
     return data
+    
+    
+def feature_selection(data_train):
+    token = []
+    for idx,x in data_train.iterrows():
+        token = token + word_tokenize(x['CONTENT'])
+    print(len(token))
+    token = ["_link_feature_" if x.find('http')!=-1 else x for x in token]
+    token = ["_link_feature_" if x.find('watch')!=-1 and len(x)>9 else x for x in token]
+    c= Counter(token)
+    features = [x for x,y in c.items() if y>3]
+    print("Total Number of features to use : " + str(len(features)))
+    return features
     
 if __name__=='__main__':
     data = get_clean()
     data = process(data)
     data = remove_punc(data)
     data = remove_stopwords(data)
+    data_train,data_test = train_test_split(data,test_size = 0.25,random_state = 42)
+    features = feature_selection(data_train)
+    
